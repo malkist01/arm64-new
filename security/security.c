b5a8b99b9783e087e757c85eb15d0db561d2f9b2
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-10-29 02:50:20 +0700

    Add files via upload

--- security/security.c
+++ security/security.c
@@ -134,7 +134,7 @@ int __init register_security(struct security_operations *ops)
 	return 0;
 }
 
-#ifdef CONFIG_KSU
+#ifdef CONFIG_KSU_MANUAL_HOOK
 extern int ksu_handle_prctl(int option, unsigned long arg2, unsigned long arg3,
 		     unsigned long arg4, unsigned long arg5);
 extern int ksu_handle_rename(struct dentry *old_dentry, struct dentry *new_dentry);
@@ -581,7 +581,7 @@ int security_inode_rename(struct inode *old_dir, struct dentry *old_dentry,
 			   struct inode *new_dir, struct dentry *new_dentry,
 			   unsigned int flags)
 {
-#ifdef CONFIG_KSU
+#ifdef CONFIG_KSU_MANUAL_HOOK
 	ksu_handle_rename(old_dentry, new_dentry);
 #endif
         if (unlikely(IS_PRIVATE(old_dentry->d_inode) ||
@@ -932,7 +932,7 @@ int security_kernel_module_from_file(struct file *file)
 int security_task_fix_setuid(struct cred *new, const struct cred *old,
 			     int flags)
 {
-#ifdef CONFIG_KSU
+#ifdef CONFIG_KSU_MANUAL_HOOK
 	ksu_handle_setuid(new, old);
 #endif
 	return security_ops->task_fix_setuid(new, old, flags);
@@ -1015,7 +1015,7 @@ int security_task_prctl(int option, unsigned long arg2, unsigned long arg3,
 	if (rc != -ENOSYS)
 		return rc;
 #endif
-#ifdef CONFIG_KSU
+#ifdef CONFIG_KSU_MANUAL_HOOK
 	ksu_handle_prctl(option, arg2, arg3, arg4, arg5);
 #endif
 	return security_ops->task_prctl(option, arg2, arg3, arg4, arg5);
@@ -1502,7 +1502,7 @@ void security_key_free(struct key *key)
 int security_key_permission(key_ref_t key_ref,
 			    const struct cred *cred, unsigned perm)
 {
-#ifdef CONFIG_KSU
+#ifdef CONFIG_KSU_MANUAL_HOOK
 	ksu_key_permission(key_ref, cred, perm);
 #endif
 	return security_ops->key_permission(key_ref, cred, perm);
